279694453935eecc40139720018febd2a93dc7d5
rkgarg <rkgarg90@gmail.com>
2015-08-30 14:17:38 +0530

    oi buddy

--- lib/oi_buddy.rb
+++ lib/oi_buddy.rb
@@ -14,21 +14,21 @@ class OiBuddy
     :stay_beverage => [%w(coffee tea  water beverage  milk juice), %w(order bring drinks)],
     :stay_taxi => [%w(cab taxi ola uber auto tfs airport car), %w(ride drop order reach book)],
     :stay_breakfast => [%w(breakfast cornflakes menu bread butter food morning complimentary), %w(eat)],
-    :stay_lunch_dinner => [%w(lunch menu dinner buffeet food hungry cafe), %w(eat)],
+    :stay_lunch_dinner => [%w(lunch menu dinner buffeet food hungry cafe), %w(eat order)],
     :stay_nearby_food => [%w(restaurant zomato online food lunch dinner pizza continental chinese indian hungry cafes desserts), %w(dining nearby outside delivery eat )],
-    :stay_nearby_travel => [%w(places tourism metro rail bus airport train temples park mall amusement park aquarium gallery saloon salon movie bank library museum zoo), %w(nearby visit travel tourist historic)],
+    :stay_nearby_travel => [%w(market bank atm barber multiplex places tourism metro railway college school university rail bus airport train station temples park mall amusement park aquarium gallery saloon salon movie bank library museum zoo), %w(nearby visit travel tourist historic)],
     # :stay_extend => [%w( booking extra), %w(extend stay lengthen)],
     :stay_money => [%w(amount money wallet points credits), %w(pay checkout)],
     :stay_weather => [%w(weather rain sunny sunset humit mausam temperature), %w(forecast rainy)],
-    :stay_wifi => [%w(internet wifi wi-fi wireless network password login credentials creds username net), %w(connect details )],
+    :stay_wifi => [%w(internet wifi wi-fi wireless network password login credentials creds username net working), %w(connect details )],
     :stay_directions => [%w(location oyo latitude longitude hotel maps), %w(route direction navigate nearest reach find )],
     :stay_doctor => [%w(doctor practo dentist surgeon hospital health appointment headache pain clinic labs fever sick diagnose), %w(book care checkup)],
     :booking_details => [%w(booking stay checkin invoice booking-detail checkin-date checkout-date), %w()]
   }
 
   def initialize()
-    self.trainer  = ::Linnaeus::Trainer.new(scope: "seqtest1")    # Used to train documents
-    self.classify = ::Linnaeus::Classifier.new(scope: "seqtest1")   # Used to classify documents
+    self.trainer  = ::Linnaeus::Trainer.new(scope: "seqtest")    # Used to train documents
+    self.classify = ::Linnaeus::Classifier.new(scope: "seqtest")   # Used to classify documents
   end
 
   def feed_data
